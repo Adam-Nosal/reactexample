de46c46510af4c8f5f119dc0d550a5098856ec44
adam.nosal <nosal.adam11@gmail.com>
2021-09-30 21:32:48 +0200

    initreact

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p>Hello !</p>
+     <h1>wtf</h1>
+     <div>
+       <p>div2</p>
+     </div>
+        <div>
+         <a href="https://wp.pl">wp</a> 
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
