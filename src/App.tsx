8f38ca6caf8b4ace040eb0802dfa040d23d23d48
Adam.Nosal <nosal.adam11@gmail.com>
2021-09-30 22:17:20 +0200

    image commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Media, { MediaVideo } from "./Media";
 
 function App() {
   return (
     <div className="App">
-     <p>Hello !</p>
-     <h1>wtf</h1>
-     <div>
-       <p>div2</p>
-     </div>
-        <div>
-         <a href="https://wp.pl">wp</a> 
-        </div>
-
+      <p>Hello !</p>
+      <h1>wtf</h1>
+      <div>
+        <p>div2</p>
+      </div>
+      <div>
+        <a href="https://wp.pl">wp</a>
+      </div>
+      <Media />
+      <div>
+        <MediaVideo/>
+      </div>
     </div>
   );
 }
---
+++ src/Media.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import  logo from '../src/Image/image.ico';
+
+
+
+export default function Media() {
+  return (
+      <div>
+          <img src={logo} alt="unlucky"/>
+      </div>
+  )
+}
+
+export  function MediaVideo() {
+    return (
+        <div>
+            <img src={logo} alt="unlucky"/>
+        </div>
+    )
+  }
+  
