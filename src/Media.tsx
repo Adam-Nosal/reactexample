76f345b84c8b0eb5cf201ebfde42572acd0d55ea
Adam.Nosal <nosal.adam11@gmail.com>
2021-09-30 22:23:18 +0200

    master

--- src/Media.tsx
+++ src/Media.tsx
@@ -1,21 +1,32 @@
 import React from "react";
-import  logo from '../src/Image/image.ico';
-
-
+import logo from "../src/Image/image.ico";
 
 export default function Media() {
   return (
-      <div>
-          <img src={logo} alt="unlucky"/>
-      </div>
-  )
+    <div>
+      <img src={logo} alt="unlucky" />
+    </div>
+  );
 }
 
-export  function MediaVideo() {
-    return (
-        <div>
-            <img src={logo} alt="unlucky"/>
-        </div>
-    )
-  }
-  
+export function MediaVideo() {
+  return (
+    <div>
+      <table> 
+        <thead>
+          <tr>
+            <th colSpan={3}>ex tbl</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>lol</td>
+            <td>lol2</td>
+            <td>lol3</td>
+          </tr>
+        </tbody>
+      </table>
+      <img src={logo} alt="unlucky" />
+    </div>
+  );
+}
